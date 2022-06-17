4ec9703d557866af4e44f5882daa02c380a2fe3b
Ninmay <ad438410@students.mimuw.edu.pl>
2022-06-17 21:57:04 +0000

    Minor changes to gaps and filepicker (audio files)

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const int startwithgaps[]    = {0, 1, 0, 0, 1};	/* 1 means gaps are used by default, this can be customized for each tag */
-static const unsigned int gappx[]   = {85, 85, 85, 85, 85};   /* default gap between windows in pixels, this can be customized for each tag */
+static const unsigned int gappx[]   = {55, 55, 55, 55, 55};   /* default gap between windows in pixels, this can be customized for each tag */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
