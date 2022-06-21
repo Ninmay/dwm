76f8a4fa4e063e5e4f6968af63d4a2b2b43b8815
Ninmay <ad438410@students.mimuw.edu.pl>
2022-06-21 15:51:15 +0000

    new package directory, added maria package

--- include/palette.h
+++ include/palette.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * This is Maria's colorscheme for dwm to use.
+ * This is Alicia's colorscheme for dwm to use.
  * 
  * @date 2022 
  */
@@ -8,16 +8,16 @@
 #ifndef __PALLETE_H__
 #define __PALLETE_H__
 
-static const char col_letter1[]     = "#bbbbbb";
-static const char col_letter2[]     = "#eeeeee";
-static const char col_background[]  = "#1b1a15";
-static const char col_foreground[]  = "#312d2e"; // maria_hat
+static const char col_letter1[]     = "#1b1a15";
+static const char col_letter2[]     = "#1b1a15";
+static const char col_background[]  = "#fad8ce";
+static const char col_foreground[]  = "#a2cbdf"; // blue_sky
 static const char col_black[]       = "#000000";
-static const char col_border[]      = "#4a543c"; // doll_eyes 
+static const char col_border[]      = "#36479f"; // boat_color
 static const char *colors[][3]      = {
-	/*               fg           bg               border       */
-	[SchemeNorm] = { col_letter1, col_background,  col_black     },
-	[SchemeSel]  = { col_letter2, col_foreground,  col_border    },
+	/*               fg           bg               border    */
+	[SchemeNorm] = { col_letter1, col_background,  col_black  },
+	[SchemeSel]  = { col_letter2, col_foreground,  col_border },
 };
 
 #endif /* __PALLETE_H__ */
