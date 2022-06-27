dd1db3dc339da7cbc54452c42be268f2b05eeee6
Ninmay <ad438410@students.mimuw.edu.pl>
2022-06-27 13:26:59 +0000

    next

--- dwm.c
+++ dwm.c
@@ -662,7 +662,7 @@ createmon(void)
 	m->topbar = topbar;
 	m->lt[0] = &layouts[1];
 	m->lt[1] = &layouts[0];
-	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
+	strncpy(m->ltsymbol, layouts[1].symbol, sizeof m->ltsymbol);
 	m->pertag = ecalloc(1, sizeof(Pertag));
 	m->pertag->curtag = m->pertag->prevtag = 1;
 
@@ -671,7 +671,6 @@ createmon(void)
 		m->pertag->mfacts[i] = m->mfact;
 
 		m->pertag->ltidxs[i][0] = m->lt[0];
-		m->pertag->ltidxs[i][0] = startwithlayout[0];
 		m->pertag->ltidxs[i][1] = m->lt[1];
 		m->pertag->sellts[i] = m->sellt;
 
