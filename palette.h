74ce0fe653e3f67c2525a84b80642375be03a648
Ninmay <ad438410@students.mimuw.edu.pl>
2022-07-01 13:23:51 +0200

    more scripts

--- palette.h
+++ palette.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * This is Beatrice's colorscheme for dwm to use.
+ * This is Maria's colorscheme for dwm to use.
  * 
  * @date 2022 
  */
@@ -10,12 +10,12 @@
 
 static const char col_letter1[]     = "#bbbbbb";
 static const char col_letter2[]     = "#eeeeee";
-static const char col_background[]  = "#362221";
-static const char col_foreground[]  = "#400c19"; // trice_dress
+static const char col_background[]  = "#1b1a15";
+static const char col_foreground[]  = "#312d2e"; // maria_hat
 static const char col_black[]       = "#000000";
-static const char col_border[]      = "#4f200e"; // trice_hair
+static const char col_border[]      = "#4a543c"; // doll_eyes 
 static const char *colors[][3]      = {
-	/*               fg           bg               border    */
+	/*               fg           bg               border       */
 	[SchemeNorm] = { col_letter1, col_background,  col_black  },
 	[SchemeSel]  = { col_letter2, col_foreground,  col_border },
 };
